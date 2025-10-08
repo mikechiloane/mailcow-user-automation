631ae7687127f3bbc75c9c722572094223e77dc1
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-08 23:24:50 +0200

    implementation of the simple reader

--- src/main/java/com/recceda/csv/CsvReader.java
+++ src/main/java/com/recceda/csv/CsvReader.java
@@ -2,9 +2,10 @@ package com.recceda.csv;
 
 import com.recceda.elements.User;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface CsvReader {
-    List<User> read();
+    List<User> read() throws IOException;
     int countRows();
 }
--- src/main/java/com/recceda/csv/SimpleCsvReader.java
+++ src/main/java/com/recceda/csv/SimpleCsvReader.java
@@ -2,20 +2,26 @@ package com.recceda.csv;
 
 import com.recceda.elements.User;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class SimpleCsvReader implements CsvReader{
 
-    private final File csvFile;
+    private final Path csvPath;
 
-    public SimpleCsvReader(String csvPath){
-
+    public SimpleCsvReader(String csvPath) {
+        this.csvPath = Paths.get(csvPath);
     }
 
     @Override
-    public List<User> read() {
-        return null;
+    public List<User> read() throws IOException {
+        List<String> lines = Files.readAllLines(csvPath);
+        if(lines.size()<=1) return List.of();
+
+        return lines.
     }
 
     @Override
